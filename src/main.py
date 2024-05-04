4c0db9280a3346f0e022f61ea28eb3815606ea78
maxshlain <maxshlain@gmail.com>
2024-05-04 20:27:02 +0300

    refactor: Improve keyboard event handling and translation logic

--- src/main.py
+++ src/main.py
@@ -1,45 +1,46 @@
 import keyboard
 import time
 
-# # A dictionary to map English letters to Russian
-# eng_to_rus = {
-#     'a': 'ф', 'b': 'и', 'c': 'с', 'd': 'в', 'e': 'у', 'f': 'а', 'g': 'п', 'h': 'р', 'i': 'ш', 'j': 'о', 'k': 'л', 'l': 'д', 'm': 'ь', 'n': 'т', 'o': 'щ', 'p': 'з', 'q': 'й', 'r': 'к', 's': 'ы', 't': 'е', 'u': 'г', 'v': 'м', 'w': 'ц', 'x': 'ч', 'y': 'н', 'z': 'я',
-#     'A': 'Ф', 'B': 'И', 'C': 'С', 'D': 'В', 'E': 'У', 'F': 'А', 'G': 'П', 'H': 'Р', 'I': 'Ш', 'J': 'О', 'K': 'Л', 'L': 'Д', 'M': 'Ь', 'N': 'Т', 'O': 'Щ', 'P': 'З', 'Q': 'Й', 'R': 'К', 'S': 'Ы', 'T': 'Е', 'U': 'Г', 'V': 'М', 'W': 'Ц', 'X': 'Ч', 'Y': 'Н', 'Z': 'Я'
-# }
-
 # The last word typed
 last_word = ""
 
 # The time the last caps lock was pressed
 last_caps_time = 0
 
+def reset_last_word():
+    global last_word
+    last_word = ""
+
+def check_caps_lock_press(event):
+    global last_caps_time
+    if time.time() - last_caps_time < 0.5:
+        delete_last_word()
+        type_translated_word()
+
+def delete_last_word():
+    global last_word
+    keyboard.send('backspace', repeat=len(last_word))
+
+def type_translated_word():
+    print("Type the translated word")
+
 def on_key(event):
-    global last_word, last_caps_time
+    print(event)
     if event.name == 'space':
         print("If the key is space, reset the last word")
-        last_word = ""
-    elif event.name == 'caps lock' and event.event_type == 'down':
-        print("If caps lock is pressed twice quickly, translate the last word")
-        if time.time() - last_caps_time < 0.5:
-            print("Delete the last word")
-            keyboard.send('backspace', repeat=len(last_word))
-            print("Type the translated word")
-            for char in last_word:
-                if char in eng_to_rus:
-                    keyboard.write(eng_to_rus[char])
-                else:
-                    keyboard.write(char)
-            last_word = ""
-        last_caps_time = time.time()
-    elif event.event_type == 'down':
-    # else:
-        # If the key is alphanumeric, add it to the last word
-        print("If the key is alphanumeric, add it to the last word")
-        print(f"The key is {event.scan_code}")
-        # last_word += event.name
-        print("The last word is", last_word)
+        reset_last_word()
+        return
+    
+    if event.name == 'caps lock' and event.event_type == 'down':
+        check_caps_lock_press(event)
+        return
+    
+    if event.event_type == 'down':
+        print("If the key is down, add the key to the last word")
+        add_key_to_last_word(event)
 
-# Start listening to keyboard events
-keyboard.hook(on_key)
-print("Listening to keyboard events")
-keyboard.wait()
+if __name__ == "__main__":    
+    # Start listening to keyboard events
+    keyboard.hook(on_key)
+    print("Listening to keyboard events")
+    keyboard.wait()
