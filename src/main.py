3beb2a2bf387a9118239748f7e1624c0784b1454
maxshlain <maxshlain@gmail.com>
2024-05-05 17:19:22 +0300

    refactor: Improve keyboard event handling and translation logic

--- src/main.py
+++ src/main.py
@@ -2,57 +2,84 @@ import time
 import pynput
 
 last_caps_lock_press_time = 0
-double_press_interval = 0.5  # seconds
+double_press_interval = 500  # seconds
 pressed_keys_since_whitespace = [] # Step 1: Initialize the variable to store pressed keys
 keyboard = pynput.keyboard.Controller()
+
+def is_escape(key):
+    return key == pynput.keyboard.Key.esc
+
+def is_signal(key):
+    return key == pynput.keyboard.Key.caps_lock
+
+def milliseconds_since_last_signal(last_caps_lock_press_time):
+    interval = (time.time() - last_caps_lock_press_time) * 1000
+    return int(interval)
+
+def on_signal(key):
+    global last_caps_lock_press_time, pressed_keys_since_whitespace  # Include the new global variable
+    
+    if last_caps_lock_press_time == 0:
+        last_caps_lock_press_time = time.time()
+        return
+        
+    if len(pressed_keys_since_whitespace) == 0:
+        last_caps_lock_press_time = time.time()
+        return
+
+    current_interval = milliseconds_since_last_signal(last_caps_lock_press_time)
+    print(f"Current interval: {current_interval}")
+    if current_interval > double_press_interval:
+        last_caps_lock_press_time = time.time()
+        return
+
+    if last_caps_lock_press_time != 0 and current_interval < double_press_interval:
+        last_caps_lock_press_time = time.time()  # Reset to avoid triple press detection
+        print(f"Double {current_interval} Caps lock pressed")
+        print(f"Keys since last whitespace: {pressed_keys_since_whitespace}")
+        for i in pressed_keys_since_whitespace:
+            try:
+                if i.char:
+                    keyboard.type(i.char)
+            except AttributeError:
+                # Handle special keys here if needed
+                pass
+        pressed_keys_since_whitespace = []
+    return True
+
+def is_reset(key):
+    return key == pynput.keyboard.Key.space or \
+    key == pynput.keyboard.Key.enter or \
+    key == pynput.keyboard.Key.backspace
+
+def reset():
+    global pressed_keys_since_whitespace
+    print(f"Keys since last whitespace: {pressed_keys_since_whitespace}")
+    pressed_keys_since_whitespace = []  # Reset the variable
+
+def collect(key):
+    global pressed_keys_since_whitespace
+    try:
+        pressed_keys_since_whitespace.append(key)
+    except AttributeError:
+        # Handle special keys here if needed
+        pass
 
 def on_release(key):
     global last_caps_lock_press_time, pressed_keys_since_whitespace  # Include the new global variable
     
-    if key == pynput.keyboard.Key.esc:
+    if is_escape(key):
         return False
     
-    if key == pynput.keyboard.Key.caps_lock:
-
-        if last_caps_lock_press_time == 0:
-            last_caps_lock_press_time = time.time()
-            return True
-        
-        if len(pressed_keys_since_whitespace) == 0:
-            last_caps_lock_press_time = time.time()
-            return True
-
-        current_interval = time.time() - last_caps_lock_press_time
-        if current_interval > double_press_interval:
-            last_caps_lock_press_time = time.time()
-            return True
-
-        if last_caps_lock_press_time != 0 and current_interval < double_press_interval:
-            last_caps_lock_press_time = time.time()  # Reset to avoid triple press detection
-            print(f"Double {current_interval} Caps lock pressed")
-            print(f"Keys since last whitespace: {pressed_keys_since_whitespace}")
-            for i in pressed_keys_since_whitespace:
-                try:
-                    if i.char:
-                        keyboard.type(i.char)
-                except AttributeError:
-                    # Handle special keys here if needed
-                    pass
-            pressed_keys_since_whitespace = []
+    if is_signal(key):
+        on_signal(key)
         return True
 
-    # Step 2: Reset pressed_keys_since_whitespace on whitespace
-    if key == pynput.keyboard.Key.space or key == pynput.keyboard.Key.enter:
-        print(f"Keys since last whitespace: {pressed_keys_since_whitespace}")
-        pressed_keys_since_whitespace = []  # Reset the variable
-    else:
-        # Step 3: Append non-whitespace keys to pressed_keys_since_whitespace
-        try:
-            pressed_keys_since_whitespace.append(key)
-        except AttributeError:
-            # Handle special keys here if needed
-            pass
-
+    if is_reset(key):
+        reset()
+        return True
+    
+    collect(key)
     return True
 
 # Collect events until released
